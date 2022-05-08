a2f84c688ec80ce8fa67504d0baf67507126c999
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-05-09 01:40:36 +0530

    feat-changes

---
+++ src/Route/Route.jsx
@@ -0,0 +1,10 @@
+// import{Routes, Route} from "react-router"
+import { Home } from "../pages/pages"
+
+export default function Route(){
+    return(
+        // <Routes>
+        //     <Route path="/" element={<Home />}/>
+        // </Routes>
+    )
+}
